db146001697f3a28560c71c1f72446a5dd99b70a
Vladimir Orlov <volodymyr.orlov@gmail.com>
2013-01-09 18:25:28 +0200

    SourceInputStream refactored

--- scala-aws-core/src/test/scala/com/codeminders/scalaws/helpers/io/SourceInputStreamTests.scala
+++ scala-aws-core/src/test/scala/com/codeminders/scalaws/helpers/io/SourceInputStreamTests.scala
@@ -8,59 +8,41 @@ import scala.io.Source
 import org.apache.commons.io.IOUtils
 import java.io.InputStream
 import java.io.File
+import java.io.FileOutputStream
 
 @RunWith(classOf[JUnitRunner])
 class SourceInputStreamTests extends FunSuite {
-  
-  def nextFibonacciNumber(n: Int): Int = n match {
 
-    case 0 | 1 => n
-    case _ => nextFibonacciNumber(n - 1) + nextFibonacciNumber(n - 2)
-  }
-  
-  def initializeByteArrayWithFibonacciNumbers(size: Int): Array[Byte] = {
-    val arr = Array.ofDim[Byte](size)
-    (0 to size).foreach{
-      i => 
-        arr(i) = nextFibonacciNumber(i).toByte
-    }
-    arr
-  }
-
-  test("Verify SourceInputStream for strings"){
-    val s = IOUtils.toString(new SourceInputStream(Source.fromString("Данные")))
-    assert("Данные" === s)
-  }
-  
-  test("Verify SourceInputStream for files"){
-    val expectedIS = getClass.getClassLoader().getResourceAsStream("50KText")
-    val actualIS = new SourceInputStream(Source.fromInputStream(getClass.getClassLoader().getResourceAsStream("50KText")))
-    assertInputStreamsEqual(expectedIS, actualIS)
-  }
-  
-  test("Verify SourceInputStream read method"){
+  test("Verify SourceInputStream read methods") {
     val testData = "InputStream Data"
-    val expectedBuffer = testData.getBytes() 
+    val expectedBuffer = testData.getBytes()
     val is = new SourceInputStream(Source.fromString(testData))
     val buffer = Array.ofDim[Byte](expectedBuffer.length)
-    // read first half of data
+    // read second half of data
     is.read(buffer, testData.length() / 2, 1024)
     assert(buffer.drop(testData.length() / 2).zip(expectedBuffer).forall(e => e._1 == e._2))
-    // read second half of data
-    is.read(buffer, 0, 1024)
-    assert(buffer.zip(expectedBuffer.drop(testData.length() / 2)).forall(e => e._1 == e._2))
-    
+    // read first half of data
+    assert(expectedBuffer.drop(testData.length() / 2).forall(e => e == is.read()))
   }
-  
-  def assertInputStreamsEqual(expected: InputStream, actual:InputStream) {
+
+  test("Verify SourceInputStream correctness") {
+    assertInputStreamsEqual(
+      getClass.getClassLoader().getResourceAsStream("50KText"),
+      new SourceInputStream(Source.fromInputStream(getClass.getClassLoader().getResourceAsStream("50KText"))))
+    assertInputStreamsEqual(
+      getClass.getClassLoader().getResourceAsStream("UTF-8"),
+      new SourceInputStream(Source.fromInputStream(getClass.getClassLoader().getResourceAsStream("UTF-8"))))
+  }
+
+  def assertInputStreamsEqual(expected: InputStream, actual: InputStream) {
     val actualBuffer = Array.ofDim[Byte](1024 * 8)
     val expectedBuffer = Array.ofDim[Byte](1024 * 8)
     var bytesRead = 0
-    do{
-      bytesRead = expected.read(expectedBuffer)
-      assert(bytesRead === actual.read(actualBuffer))
+    do {
+      bytesRead = actual.read(actualBuffer)
+      assert(bytesRead === expected.read(expectedBuffer))
       assert(actualBuffer.zip(expectedBuffer).forall(e => e._1 == e._2))
-    }while(bytesRead > 0)
+    } while (bytesRead > 0)
   }
-  
+
 }
